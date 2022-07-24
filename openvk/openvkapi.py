d909807ca671d6a0144777ef4990c7b45013a955
Alyona Lisicyna <105191100+Parliskaya@users.noreply.github.com>
2022-07-24 15:07:15 +0300

    Auth in token

--- openvk/openvkapi.py
+++ openvk/openvkapi.py
@@ -7,16 +7,23 @@ class openvkapi:
     """Авторизация пользователя"""
 
     @staticmethod
-    def auth(login: str, password: str, instance='openvk.su', code=0):
-        response = http.get(f'https://{instance}/token?username={login}&password={password}&code={code}&grant_type=password').json()
-        token = str(response['access_token'])
-        user_id = int(response['user_id'])
-        response = {
-            'instance': instance,
-            'token': token,
-            'id': user_id
-        }
-        return response
+    def auth(login='', password='', code=0, instance='openvk.su', token=''):
+        if token == '':
+            response = http.get(f'https://{instance}/token?username={login}&password={password}&code={code}&grant_type=password').json()
+            token = str(response['access_token'])
+            user_id = int(response['user_id'])
+            response = {
+                'instance': instance,
+                'token': token,
+                'id': user_id
+            }
+            return response
+        elif token != '':
+            response = {
+                'instance': instance,
+                'token': token
+            }
+            return response
 
     @staticmethod
     def pam():
